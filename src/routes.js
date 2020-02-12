d183f74ebfadc8b54066c5a9e0167bfa4f305b2e
GabrielFagundes <aluzgabriel@hotmail.com>
2020-02-12 00:48:02 -0300

    Add User update and validations with yup

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,20 @@ import { Router } from 'express';
 import UserController from './app/constrollers/UserController';
 import SessionController from './app/constrollers/SessionController';
 
-const routes = new Router();
+import authMiddleware from './app/middlewares/auth';
 
-routes.post('/users', UserController.store);
-routes.post('/sessions', SessionController.store);
+const routes = new Router();
 
 routes.get('/', (req, res) => {
   res.json({ message: 'Started' });
 });
+
+routes.post('/sessions', SessionController.store);
+routes.post('/users', UserController.store);
+
+// Routes bellow this middleware will pass only if the user is logged in
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
